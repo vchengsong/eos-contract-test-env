a8b5d294bc59b0b94502acd1dfb0ec54fd04ca50
song <vchengsong@gmail.com>
2018-11-26 12:53:33 +0800

    add killall nodeos

--- nodeos_start.sh
+++ nodeos_start.sh
@@ -3,6 +3,9 @@
 if [ "$1" == "re" ];then
     rm -rf /Users/song/tmp/eosio/data/*
 fi
+
+killall nodeos
+
 nodeos -e -p eosio -d /Users/song/tmp/eosio/data --config-dir /Users/song/tmp/eosio/config  \
     --plugin eosio::chain_api_plugin --plugin eosio::producer_plugin  \
     --plugin eosio::producer_api_plugin --plugin eosio::history_api_plugin  \
