cff4c7809114b3c45302de0027c299a880579225
song <vchengsong@gmail.com>
2018-11-26 12:08:40 +0800

    v1.0.0

--- init.sh
+++ init.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
 
-
-#CONTRACTS_DIR=/Users/song/tmp/contracts
-#CONTRACTS_DIR=/Code/github.com/vchengsong/eos/build/contracts
 CONTRACTS_DIR=/Code/github.com/vchengsong/eosio.contracts/build
-
 
 WALLET_DIR=/Users/song/tmp/eosio/eosio-wallet
 
@@ -12,14 +8,28 @@ start_keosd(){
     killall keosd 2>/dev/null
     rm -rf ${WALLET_DIR}
     mkdir ${WALLET_DIR}
-    nohup keosd -d ${WALLET_DIR} 2>/dev/null &
+    nohup keosd -d ${WALLET_DIR} --unlock-timeout 1000000 1>/dev/null  2>/dev/null &
 }
 start_keosd
 
 
 create_wallet(){
+    rm -rf ~/eosio-wallet/
     cleos wallet create --to-console
     cleos wallet import --private-key 5KQwrPbwdL6PhXujxW37FSSQZ1JiwsST4cqQzDeyXtP79zkvFD3
 }
 create_wallet
 
+new_keys(){
+    str=`cleos create key --to-console`
+    pri_key=`echo $str | cut -d' ' -f 3`
+    pub_key=`echo $str | cut -d' ' -f 6`
+}
+
+import_key(){
+    cleos wallet import --private-key $1
+}
+
+#create_system_account(){
+#    new_keys && cleos create account eosio $1 $pub_key -p eosio && import_key ${pri_key}
+#}
--- task.sh
+++ task.sh
@@ -1,170 +1,161 @@
 #!/usr/bin/env bash
 
-. init.sh
+. ./init_system_contracts.sh
 
-set -x
+. ./task_ibc_test.sh
 
-import_key(){
-    cleos wallet import --private-key $1
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+eosio_test_test(){
+
+    a=eostest12345
+    create_one ${a}
+    cleos set contract ${a} ${CONTRACTS_DIR}/eosio.test -x 1000 -p ${a}
+
+    cleos transfer eosfirstacnt ${a}  "1.2345 EOS" -p eosfirstacnt
+
+
+    cleos push action ${a} transfer '["eosfirstacnt","eostest12345","1.2356 EOS","memo"]' -p eosfirstacnt
 }
 
-new_keys(){
-    str=`cleos create key --to-console`
-    pri_key=`echo $str | cut -d' ' -f 3`
-    pub_key=`echo $str | cut -d' ' -f 6`
-}
 
-# usage: create_account account_name
-create_account(){
-    new_keys && cleos create account eosio $1 $pub_key -p eosio
-    import_key $pri_key
-}
 
-steps(){
-    # step 1: set contract eosio.bios
-    cleos set contract eosio ${CONTRACTS_DIR}/eosio.bios -p eosio
 
-    # step 2: create system accounts
-    sleep 1
-    for account in eosio.aaa eosio.bpay eosio.msig eosio.names eosio.ram eosio.ramfee eosio.saving eosio.stake eosio.token eosio.vpay
-    do
-        echo -e "\n creating $account \n";
-        cleos create account eosio ${account} EOS6MRyAjQq8ud7hVNYcfnVPJqcVpscN5So8BhtHuGYqET5GDW5CV -p eosio -x 1000
-        sleep .2
-    done
 
-    # step 3: set contract
-    echo step 3
-    cleos set contract eosio.token ${CONTRACTS_DIR}/eosio.token -p eosio.token
-    cleos set contract eosio.msig ${CONTRACTS_DIR}/eosio.msig -p eosio.msig
-
-    # step 4: create and issue token
-    echo step 4
-    cleos push action eosio.token create '["eosio", "10000000000.0000 EOS"]' -p eosio.token
-    cleos push action eosio.token issue '["eosio",  "1000000000.0000 EOS", "cryptokylin"]' -p eosio
-
-    # setp 5: setting privileged account for eosio.msig
-#    cleos push action eosio setpriv '{"account": "eosio.msig", "is_priv": 1}' -p eosio
-
-    # step 6: set contract eosio.system
-    cleos set contract eosio ${CONTRACTS_DIR}/eosio.system -x 1000 -p eosio
-
-    cleos push action eosio init '[0, "4,EOS"]' -p eosio
-
-}
-steps
-
-sleep .2
-create_eosfirstacnt(){
-    echo "create account."
-    new_keys
-    cleos system newaccount \
-         --stake-net "100.0000 EOS" --stake-cpu "100.0000 EOS" --buy-ram "100.0000 EOS" \
-         eosio eosfirstacnt $pub_key $pub_key -p eosio --transfer
-    cleos transfer eosio eosfirstacnt "10000.0000 EOS"
-    import_key $pri_key
-}
-create_eosfirstacnt
 
 #cleos push action eosio newaccount '{"creator":"eosio","name":"eosstore1111","owner":{"threshold":1,"keys":[{"key":"EOS6yuxPfeaDrAxgHCh6H4jT2kgGvuy11hPgfEPksa4SwxGr3ZC4d","weight":1}],"accounts":[],"waits":[]},"active":{"threshold":1,"keys":[{"key":"EOS6yuxPfeaDrAxgHCh6H4jT2kgGvuy11hPgfEPksa4SwxGr3ZC4d","weight":1}],"accounts":[],"waits":[]}}' -p  eosio
 
-create_one(){
-    new_keys
-    cleos system newaccount \
-     --stake-net "1.0000 EOS" --stake-cpu "1.0000 EOS" --buy-ram "1.0000 EOS" \
-     eosfirstacnt eosstoreff$1 $pub_key $pub_key -p eosfirstacnt
+namelist(){
+    cleos push action eosio namelist '["actor_blacklist", "insert", ["eosstoreff1a","eosstoreff1b","eosstoreff1c"]]'  -p eosio
+    #cleos get table eosio eosstoreff1a userres
+    #cleos get table eosio eosio minguar
 }
-
-for i in 1a 1b 1c 1d 1e 1f 1g 1h 1i; do create_one ${i}; done
-
-cleos push action eosio namelist '["actor_blacklist", "insert", ["eosstoreff1a","eosstoreff1b","eosstoreff1c"]]'  -p eosio
-
-#cleos get table eosio eosstoreff1a userres
-#cleos get table eosio eosio minguar
-
-cleos get account eosstoreff1a
-cleos push action eosio setminguar '[1,1,1]'   -p eosio
-
 
 hallo(){
+    cleos get account eosstoreff1a
+    cleos push action eosio setminguar '[1,1,1]'   -p eosio
+    cleos push action eosio setlists '{
+      "params": {
+          "actor_blacklist":["111","222","333","444","555","aaa","bbb"],
+          "contract_blacklist":["111","222"],
+          "resource_greylist":["111"]
+        }
+    }' -p eosio
 
-cleos push action eosio setlists '{
-  "params": {
-      "actor_blacklist":["111","222","333","444","555","aaa","bbb"],
-      "contract_blacklist":["111","222"],
-      "resource_greylist":["111"]
-    }
-}' -p eosio
+    cleos push action eosio setlists '{
+      "params": {
+          "actor_blacklist":[],
+          "contract_blacklist":[],
+          "resource_greylist":["111","222"]
+        }
+    }' -p eosio
 
+    cleos push action eosio setlists '{
+      "params": {
+          "actor_blacklist": [
+            "111",
+            "222",
+            "2221"
+          ],
+          "contract_blacklist": [
+            "111",
+            "222",
+            "2221"
+          ],
+          "resource_greylist": [
+            "111",
+            "222"
+          ]
+        }
+    }' -p eosio
 
-cleos push action eosio setlists '{
-  "params": {
-      "actor_blacklist":[],
-      "contract_blacklist":[],
-      "resource_greylist":["111","222"]
-    }
-}' -p eosio
-
-
-
-cleos push action eosio setlists '{
-  "params": {
-      "actor_blacklist": [
-        "111",
-        "222",
-        "2221"
-      ],
-      "contract_blacklist": [
-        "111",
-        "222",
-        "2221"
-      ],
-      "resource_greylist": [
-        "111",
-        "222"
-      ]
-    }
-}' -p eosio
-
-
-
-
-cleos get table eosio eosio lists
-
-
-cleos push action eosio setpriv '{"account": "eosio.msig", "is_priv": 1}' -p eosio
-
-cleos get table eosio eosio cntlrcfg
-
-
-
-cleos push action eosio setcontcfg '{
-  "params": {
-      "a": 101
-    }
-}' -p eosio
-
+    cleos get table eosio eosio lists
+    cleos push action eosio setpriv '{"account": "eosio.msig", "is_priv": 1}' -p eosio
+    cleos get table eosio eosio cntlrcfg
+    cleos push action eosio setcontcfg '{
+      "params": {
+          "a": 101
+        }
+    }' -p eosio
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
